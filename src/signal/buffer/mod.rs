b3bc24eb1185054c17bc7de7c2b85039bb4fb71b
Vaibhav Gopal <vabsgop@gmail.com>
2025-03-24 05:05:06 -0400

    added op macro

--- src/signal/buffer/mod.rs
+++ src/signal/buffer/mod.rs
@@ -22,12 +22,182 @@ impl<T: Unit> IntoIterator for Buffer<T> {
 
 // std::ops Implementations ===========================================
 
-impl<T: Unit> Add for Buffer<T> {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self::Output {
-        todo!()
-    }
+macro_rules! impl_buffer_binop_and_assign {
+    ($trait:ident, $method:ident, $assign_trait:ident, $assign_method:ident, $op:expr) => {
+        use std::ops::*;
+
+        // Buffer + Buffer
+        impl<T> $trait for Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            type Output = Buffer<T>;
+            fn $method(self, rhs: Self) -> Self::Output {
+                assert_eq!(self.len(), rhs.len(), "Buffers must match");
+                let data = self
+                    .data
+                    .into_iter()
+                    .zip(rhs.data.into_iter())
+                    .map($op)
+                    .collect();
+                Buffer { data }
+            }
+        }
+
+        // &Buffer + &Buffer
+        impl<'a, 'b, T> $trait<&'b Buffer<T>> for &'a Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            type Output = Buffer<T>;
+            fn $method(self, rhs: &'b Buffer<T>) -> Self::Output {
+                assert_eq!(self.len(), rhs.len(), "Buffers must match");
+                let data = self
+                    .data
+                    .iter()
+                    .copied()
+                    .zip(rhs.data.iter().copied())
+                    .map($op)
+                    .collect();
+                Buffer { data }
+            }
+        }
+
+        // Buffer + &Buffer
+        impl<'b, T> $trait<&'b Buffer<T>> for Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            type Output = Buffer<T>;
+            fn $method(self, rhs: &'b Buffer<T>) -> Self::Output {
+                assert_eq!(self.len(), rhs.len(), "Buffers must match");
+                let data = self
+                    .data
+                    .into_iter()
+                    .zip(rhs.data.iter().copied())
+                    .map($op)
+                    .collect();
+                Buffer { data }
+            }
+        }
+
+        // Buffer + &mut Buffer
+        impl<'b, T> $trait<&'b mut Buffer<T>> for Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            type Output = Buffer<T>;
+            fn $method(self, rhs: &'b mut Buffer<T>) -> Self::Output {
+                assert_eq!(self.len(), rhs.len(), "Buffers must match");
+                let data = self
+                    .data
+                    .into_iter()
+                    .zip(rhs.data.iter().copied())
+                    .map($op)
+                    .collect();
+                Buffer { data }
+            }
+        }
+
+        // &Buffer + Buffer
+        impl<'a, T> $trait<Buffer<T>> for &'a Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            type Output = Buffer<T>;
+            fn $method(self, rhs: Buffer<T>) -> Self::Output {
+                assert_eq!(self.len(), rhs.len(), "Buffers must match");
+                let data = self
+                    .data
+                    .iter()
+                    .copied()
+                    .zip(rhs.data.into_iter())
+                    .map($op)
+                    .collect();
+                Buffer { data }
+            }
+        }
+
+        // Buffer += Buffer
+        impl<T> $assign_trait<Buffer<T>> for Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            fn $assign_method(&mut self, rhs: Buffer<T>) {
+                assert_eq!(self.len(), rhs.len());
+                for (a, b) in self.data.iter_mut().zip(rhs.data.into_iter()) {
+                    *a = $op(*a, b);
+                }
+            }
+        }
+
+        // Buffer += &Buffer
+        impl<T> $assign_trait<&Buffer<T>> for Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            fn $assign_method(&mut self, rhs: &Buffer<T>) {
+                assert_eq!(self.len(), rhs.len());
+                for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+                    *a = $op(*a, *b);
+                }
+            }
+        }
+
+        // Buffer += &mut Buffer
+        impl<T> $assign_trait<&mut Buffer<T>> for Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            fn $assign_method(&mut self, rhs: &mut Buffer<T>) {
+                assert_eq!(self.len(), rhs.len());
+                for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+                    *a = $op(*a, *b);
+                }
+            }
+        }
+
+        // &mut Buffer += Buffer
+        impl<T> $assign_trait<Buffer<T>> for &mut Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            fn $assign_method(&mut self, rhs: Buffer<T>) {
+                assert_eq!(self.len(), rhs.len());
+                for (a, b) in self.data.iter_mut().zip(rhs.data.into_iter()) {
+                    *a = $op(*a, b);
+                }
+            }
+        }
+
+        // &mut Buffer += &Buffer
+        impl<T> $assign_trait<&Buffer<T>> for &mut Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            fn $assign_method(&mut self, rhs: &Buffer<T>) {
+                assert_eq!(self.len(), rhs.len());
+                for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+                    *a = $op(*a, *b);
+                }
+            }
+        }
+
+        // &mut Buffer += &mut Buffer
+        impl<T> $assign_trait<&mut Buffer<T>> for &mut Buffer<T>
+        where
+            T: $trait<Output = T> + Copy,
+        {
+            fn $assign_method(&mut self, rhs: &mut Buffer<T>) {
+                assert_eq!(self.len(), rhs.len());
+                for (a, b) in self.data.iter_mut().zip(rhs.data.iter()) {
+                    *a = $op(*a, *b);
+                }
+            }
+        }
+    };
 }
+
 
 // std::io Implementations ============================================
 
